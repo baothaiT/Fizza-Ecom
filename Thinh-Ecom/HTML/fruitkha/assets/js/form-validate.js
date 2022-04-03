6743f97a8151bf218fbe4f77ccfd03d341565923
baothaiT <baothai0225@gmail.com>
2022-04-04 00:17:12 +0700

    Add bg for home

--- Thinh-Ecom/HTML/fruitkha/assets/js/form-validate.js
+++ Thinh-Ecom/HTML/fruitkha/assets/js/form-validate.js
@@ -19,24 +19,24 @@ function valid_datas( f ){
 		 jQuery.ajax({
 			url: 'mail.php',
 			type: 'post',
-			data: jQuery('form#fruitkha-contact').serialize(),
+			data: jQuery('form#pizza-contact').serialize(),
 			complete: function(data) {
 				jQuery('#form_status').html(data.responseText);
-				jQuery('#fruitkha-contact').find('input,textarea').attr({value:''});
-				jQuery('#fruitkha-contact').css({opacity:1});
-				jQuery('#fruitkha-contact').remove();
+				jQuery('#pizza-contact').find('input,textarea').attr({value:''});
+				jQuery('#pizza-contact').css({opacity:1});
+				jQuery('#pizza-contact').remove();
 			}
 		});
 		jQuery('#form_status').html('<span class="loading">Sending your message...</span>');
-		jQuery('#fruitkha-contact').animate({opacity:0.3});
-		jQuery('#fruitkha-contact').find('input,textarea,button').css('border','none').attr({'disabled':''});
+		jQuery('#pizza-contact').animate({opacity:0.3});
+		jQuery('#pizza-contact').find('input,textarea,button').css('border','none').attr({'disabled':''});
 	}
 	
 	return false;
 }
 
 function notice( f ){
-	jQuery('#fruitkha-contact').find('input,textarea').css('border','none');
+	jQuery('#pizza-contact').find('input,textarea').css('border','none');
 	f.style.border = '1px solid red';
 	f.focus();
 }
