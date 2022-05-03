22459b9e5634d2b899aca0e3e2eeaf6a520a9672
Vothiquynhnhu <AMVAnimeMusicSad@gmail.com>
2022-05-03 12:46:30 +0700

    Add requirement

---
+++ Thinh-Ecom/Entities/PriceForSize.cs
@@ -0,0 +1,9 @@
+﻿namespace Thinh_Ecom.Entities
+{
+    public class PriceForSize
+    {
+        public string Id { get; set; }
+        public string SizeName { get; set; }
+        public int Price { get; set; }
+    }
+}
